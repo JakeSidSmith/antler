234905692e13869ef9c9981092f541ab8335bfb5
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-21 18:45:32 +0100

    Create Extension rule

---
+++ src/rules/Extension.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import RegexRule from '../regex-rule';
+
+class Extension extends RegexRule {
+  protected getName() {
+    return 'Extension';
+  }
+
+  protected getPart (resolvedPath: string) {
+    return path.extname(resolvedPath);
+  }
+}
+
+export default Extension;
