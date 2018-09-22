023cf3ce95ae514be5a8623941e2e3edbb5d1a13
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-22 20:01:13 +0100

    Move fileNameToPascalCase util

--- tests/rules/index.ts
+++ tests/rules/index.ts
@@ -1,27 +1,16 @@
 import fs from 'fs';
 import path from 'path';
 import * as rules from '../../src/rules';
+import { fileNameToPascalCase } from '../__helpers__/file-name-to-pascal-case';
 
-const MATCHES_WORD_START = /\b\w/g;
-const MATCHES_HYPHEN = /-/g;
 const MATCHES_INDEX_FILE = /^index\./;
-const MATCHES_EXTENSION = /\..+/;
-
-const toClassName = (value: string) => {
-  return value
-    .replace(MATCHES_EXTENSION, '')
-    .replace(MATCHES_WORD_START, (match) => {
-      return match.toUpperCase();
-    })
-    .replace(MATCHES_HYPHEN, '');
-};
 
 describe('rules', () => {
   it('should export an object containing all rules', () => {
     const rulesFiles = fs.readdirSync(path.resolve(__dirname, '../../src/rules')).filter((name) => {
       return !MATCHES_INDEX_FILE.test(name);
     });
-    const rulesClassNames = rulesFiles.map(toClassName);
+    const rulesClassNames = rulesFiles.map(fileNameToPascalCase);
 
     const exportedClassNames = Object.keys(rules);
 
