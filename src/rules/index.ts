0036f8b9bbd8deb7eb6431cc160bba1ecf90e026
Jake 'Sid' Smith <jake@dabapps.com>
2018-09-22 15:26:46 +0100

    Create NoJuniors rule

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -3,3 +3,4 @@ export { Extension } from './extension';
 export { FileName } from './file-name';
 export { Path } from './path';
 export { NoLonelyIndex } from './no-lonely-index';
+export { NoJuniors } from './no-juniors';
